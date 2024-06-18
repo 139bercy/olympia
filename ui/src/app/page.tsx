ece6edb7f80ed0ae66f79bb03940c3fe5b1acc8b
ytihianine <yanis.tihianine@finances.gouv.fr>
2024-06-18 15:05:47 +0000

    feat(ui): add input field

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "@dataesr/react-dsfr";
+import { Button, TextInput } from "@dataesr/react-dsfr";
+
 export default function Home() {
   return (
     <>
       <div>Hello World </div>
+      <TextInput></TextInput>
       <Button>HELLO</Button>
     </>
   );
