c1c7bdc51b2dabf61f02c7237b2c55fa07bd01a6
Nicolas Saint <nicolas.saint78@gmail.com>
2024-06-21 16:24:43 +0200

    fix(ui): official dsfr-react library

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Button } from "@dataesr/react-dsfr";
-export default function Home() {
-  return (
-    <>
-      <div>Hello World </div>
-      <Button>HELLO</Button>
-    </>
-  );
+import { Button } from "@codegouvfr/react-dsfr/Button";
+
+export default function Page() {
+
+    return (
+        <>
+        <h1>Hello World!</h1>
+        <Button>Click me</Button>
+        </>
+    );
+
 }
